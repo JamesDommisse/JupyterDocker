0683e7d5874d7844f46797e678d39dbd0c13ed2e
James <James.Dommisse@gmail.com>
2016-08-24 23:28:54 +0200

    add rq_example

---
+++ examples/rq/conf/worker_setting.py
@@ -0,0 +1,8 @@
+REDIS_URL = 'redis://redis_host:6379/0'
+# REDIS_HOST = redis_host
+# REDIS_PORT = 6379
+# REDIS_DB = 0
+# REDIS_PASSWORD = 'very secret'
+
+# Queues to listen on
+QUEUES = ['test_queue']
